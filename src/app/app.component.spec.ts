64ba92dd88764f6b20f8b0a675c2b17c18941dd5
Joseph-Wolf <jbone@metlife.com>
2018-07-20 09:43:15 -0400

    Implementing the hero Editor

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,49 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ FormsModule ],
       declarations: [
         AppComponent
-      ],
+        HeroesComponent
+      ]
     }).compileComponents();
   }));
+
+  beforeEach(async(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;   
+    fixture.detectChanges();
+  }));
+
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    let app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
   it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    let app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app');
   }));
+  describe('Render', () => {
+    let compiled;
+
+    beforeEach(async(() => {
+      compiled = fixture.debugElement.nativeElement;
+    }));
   it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    let expectedText = 'Welcome to app!';
+    let element = compiled.querySelector('.title');
+    expect(element.textContent).toContain(expectedText);
   }));
+  it('should render the heros tab', async(() => {
+    let element = compiled.querySelector('app-heroes');
+    expect(element).toBeTruthy();
+  }));
+  });
 });
--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { HeroesComponent } from './heroes.component';
 
 describe('HeroesComponent', () => {
@@ -8,18 +8,54 @@ describe('HeroesComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeroesComponent ]
+      declarations: [ HeroesComponent ],
+      imports: [ FormsModule ]
     })
     .compileComponents();
   }));
 
-  beforeEach(() => {
+  beforeEach(async(() => {
     fixture = TestBed.createComponent(HeroesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  }));
+
+  it('should create', async(() => {
+    expect(component).toBeTruthy();
+  }));
+
+  describe('Render', () => {
+    let compiled;
+
+    beforeEach(async(() => {
+      compiled = fixture.debugElement.nativeElement;
+    }));
+
+    it('should render the title', async(() => {
+      let expectedText = 'Windstorm';
+      let element = compiled.querySelector('.title');
+      expect(element).toBeTruthy();
+      expect(element.textContent).toContain(expectedText.toUpperCase());
+    }));
+
+    it('should render the id display', async(() => {
+      let expectedText = '1';
+      let element = compiled.querySelector('.id-display');
+      expect(element).toBeTruthy();
+      expect(element.textContent).toContain(expectedText);
+    }));
+
+    it('should render the name display', async(() => {
+      let expectedText = 'Windstorm';
+      let element = compiled.querySelector('.name-display input');
+      expect(element).toBeTruthy();
+      expect(element.value).toContain(expectedText);
+    }));
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should have a hero', async(() => {
+    expect(component.hero).toBeTruthy();
+    expect(component.hero.name).toEqual('Windstorm');
+  }));
+
 });
